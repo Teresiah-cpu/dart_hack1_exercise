203f60ab702cfcf1f1c44002178409c1ea1d6671
Teresiah-cpu <terrywambo2018@gmail.com>
2024-03-16 15:50:35 +0300

    craeted a new file

---
+++ program1.dart
@@ -0,0 +1,11 @@
+//Write a Dart program using variables to display your name, age, school, and a hobby. Print it in one logical sentence.
+void main() {
+  String name = "Teresiah Githua";
+  int age = 25;
+  String school = "PLP ";
+  String hobby = "Reading";
+
+  print("My name is $name ,I am $age years old. I am currectly studying at $school . My hobby is $hobby");
+
+  
+}
